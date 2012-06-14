586b197db6ba5f14aa5bbebfacc9e206136f2213
Julia Zhydanova <zhydanovajulia@gmail.com>
2012-06-15 00:25:38 +0300

    Add question model.

---
+++ db/migrate/20120614211750_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+    	t.integer :author
+    	t.string :subject
+    	t.integer :rate
+    	t.integer :answers_count
+    	t.integer :wath_count
+      t.timestamps
+    end
+  end
+end
