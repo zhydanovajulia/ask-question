01bd9c4e71c896cb4f82a8140be14862e7cc7b2d
Julia Zhydanova <zhydanovajulia@gmail.com>
2012-06-21 00:33:22 +0300

    Add author to answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,14 @@ class Answer < ActiveRecord::Base
   acts_as_rateable
   
   attr_accessible :user_id, :body, :rate, :question_id
+  validates :body, :presence => true
+
   belongs_to :question
   belongs_to :user
+  belongs_to :author, :class_name => "User"
 
+  def author
+  	self.user
+  end
   
 end
