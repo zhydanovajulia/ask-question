90431b7e4581fb26045bf451b75615df641028ef
Julia Zhydanova <jz@anahoret.com>
2012-06-24 19:57:55 +0300

    Order answers on the question page by rating.

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,4 +12,12 @@ class Question < ActiveRecord::Base
   def has_respond user
   	self.answers.where(:user => user)
   end
+
+  def sorted_answers
+    Answer.select('answers.*, count(rateable_id) as ratings_count').
+                where("`answers`.`question_id` = ? OR `answers`.`question_id` = null", self.id).
+                where("rateable_type = 'Answer'").
+                joins('LEFT JOIN ratings ON answers.id = rateable_id').
+                group('answers.id').order('count(rateable_id) DESC')
+  end
 end
