ee6fe5b30f952deee5ec1f02fb75949ac7ba9031
Julia Zhydanova <jz@anahoret.com>
2012-07-04 13:17:10 +0300

    Fill questions tags only when question id parameter provided.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 	include TagsHelper
 
 	def index
-		if params[:question_id]
+		if params[:question_id].present?
 			question = Question.find params[:question_id]
 			@question_tags = question.tags.map(&:name)
 		end
