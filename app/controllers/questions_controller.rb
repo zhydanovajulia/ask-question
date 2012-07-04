35b1d1b92fa51839639b6970d615c9a108dfa9d8
Julia Zhydanova <jz@anahoret.com>
2012-07-04 13:15:07 +0300

    Remove useless action.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -32,10 +32,6 @@ class QuestionsController < ApplicationController
   	end
   end
 
-  def edit
-    session[:return_to] ||= request.referer
-  end
-
   def update
   	if @question.update_attributes params[:question]
   		redirect_to question_path @question
