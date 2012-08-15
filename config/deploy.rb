fd9915b8d5e135804c2165e84f9182c3bfeebca3
Julia Zhydanova <zhydanovajulia@gmail.com>
2012-08-15 17:33:21 +0300

    Update deploy.rb file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,8 @@
+require "bundler/capistrano"    
+set :rvm_ruby_string, 'ruby-1.9.3-p125@rails3-cx'
+set :rvm_type, :system # defaults to using a user installation of rvm
+require "rvm/capistrano"
+
 set :application, "ask-question"
 set :repository,  "git@github.com:zhydanovajulia/ask-question.git"
 
@@ -5,13 +10,28 @@ set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :branch, fetch(:branch, "master")
 set :host, "maxtsap.fvds.ru"
-set :user, "root"
+set :user, "admin"
 set :deploy_to, "/var/www/apps"
-# set :use_sudo, false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
 
-role :web, "maxtsap.fvds.ru"                          # Your HTTP server, Apache/etc
-role :app, "maxtsap.fvds.ru"                  # This may be the same as your `Web` server
-role :db,  "maxtsap.fvds.ru", :primary => true # This is where Rails migrations will run
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+set :ip, 'maxtsap.fvds.ru'
+
+role :web, "#{user}@#{ip}"                          # Your HTTP server, Apache/etc
+role :app, "#{user}@#{ip}"                  # This may be the same as your `Web` server
+role :db,  "#{user}@#{ip}", :primary => true # This is where Rails migrations will run
+
+set :keep_releases, 5
+
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
+
+before "deploy:assets:precompile", :bundle_install
+
+after "deploy:update_code", :bundle_install
+
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
